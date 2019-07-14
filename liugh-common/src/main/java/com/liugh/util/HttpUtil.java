1387b0a2cf749d775e8125e18a343027fe86c0e3
liugh <53182347@qq.com>
2019-07-14 17:14:42 +0800

    工具类（fix） 加密API的滥用

--- liugh-common/src/main/java/com/liugh/util/HttpUtil.java
+++ liugh-common/src/main/java/com/liugh/util/HttpUtil.java
@@ -93,48 +93,4 @@ public final class HttpUtil {
             throw new RuntimeException(e);
         }
     }
-
-    public static String postSSL(String url, String data, String certPath, String certPass) {
-        try {
-            KeyStore clientStore = KeyStore.getInstance("PKCS12");
-            // 读取本机存放的PKCS12证书文件
-            FileInputStream instream = new FileInputStream(certPath);
-            try {
-                // 指定PKCS12的密码(商户ID)
-                clientStore.load(instream, certPass.toCharArray());
-            } finally {
-                instream.close();
-            }
-            SSLContext sslcontext = SSLContexts.custom().loadKeyMaterial(clientStore, certPass.toCharArray()).build();
-            // 指定TLS版本
-            SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslcontext, new String[]{"TLSv1"}, null,
-                SSLConnectionSocketFactory.getDefaultHostnameVerifier());
-            // 设置httpclient的SSLSocketFactory
-            CloseableHttpClient httpclient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
-            try {
-                HttpPost httpost = new HttpPost(url); // 设置响应头信息
-                httpost.addHeader("Connection", "keep-alive");
-                httpost.addHeader("Accept", "*/*");
-                httpost.addHeader("Content-Type", CONTENT_TYPE_FORM.toString());
-                httpost.addHeader("X-Requested-With", "XMLHttpRequest");
-                httpost.addHeader("Cache-Control", "max-age=0");
-                httpost.addHeader("User-Agent", DEFAULT_USER_AGENT);
-                httpost.setEntity(new StringEntity(data, "UTF-8"));
-                CloseableHttpResponse response = httpclient.execute(httpost);
-                try {
-                    HttpEntity entity = response.getEntity();
-                    String jsonStr = EntityUtils.toString(response.getEntity(), "UTF-8");
-                    EntityUtils.consume(entity);
-                    return jsonStr;
-                } finally {
-                    response.close();
-                }
-            } finally {
-                httpclient.close();
-            }
-        } catch (Exception e) {
-            logger.error("", e);
-            throw new RuntimeException(e);
-        }
-    }
 }
